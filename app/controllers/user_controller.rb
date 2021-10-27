f2c6067ed901f7197062b42f0f9aa08c8c4c3cd9
ngani faith <nganifaith@gmail.com>
2021-10-27 17:35:17 +0100

    Update signin call

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,7 +8,8 @@ class UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      knock_token = Knock::AuthToken.new payload: { sub: @user.id }
+      render json: { jwt: knock_token.token }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
